be7fb92984f38dabc45010fece8d372a51f62ee8
jbernadas <jb3rns@gmail.com>
2019-06-09 22:05:55 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+        ),
+      ),
+    ),
+  );
+}
